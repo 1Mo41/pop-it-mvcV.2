d6da7fe8931f2e5959e5e7c7d985000105f8cd60
root <kolya.gavrilov.220@gmail.com>
2023-03-28 12:01:19 +0300

    Верстка авторизированой страницы

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,24 +5,30 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/pop-it-mvc/public/style/style.css">
     <title>Pop it MVC</title>
 </head>
 <body>
 <header>
-    <nav>
-        <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
+    <nav class="header">
+        <div class="User">
+            <a>Вы зашли под пользователем: <b>(<?= app()->auth::user()->name ?>)</b></a>
+            <a class="list" href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
+        </div>
+        <div class="menu">
         <?php
         if (!app()->auth::check()):
             ?>
-            <a href="<?= app()->route->getUrl('/login') ?>">Вход</a>
-            <a href="<?= app()->route->getUrl('/signup') ?>">Регистрация</a>
+            <a class="list" href="<?= app()->route->getUrl('/login') ?>">Вход</a>
+            <a class="list" href="<?= app()->route->getUrl('/signup') ?>">Регистрация</a>
         <?php
         else:
             ?>
-            <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
+            <a class="vixod" href="<?= app()->route->getUrl('/logout') ?>">Выход </a>
         <?php
         endif;
         ?>
+        </div>
     </nav>
 </header>
 <main>
--- views/site/hello.php
+++ views/site/hello.php
@@ -1,1 +1,26 @@
-<h2><?= $message ?? ''; ?></h2>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/pop-it-mvc/public/style/style.css">
+    <title>Pop it MVC</title>
+</head>
+<body>
+<main>
+<h2 class="centr">Добавить сотрудника</h2>
+<div class="zapros">
+<form method="get">
+    <label>ФИО</label>
+    <input type="text" name="flmname">
+    <label>Подразделение <input type="text" name="subdivision"></label>
+    <label>Предприятие<input type="text" name="company"></label>
+    <label>Возраст<input type="number" name="age"></label>
+    <label>Состав <input type="text" name="compound"></label>
+</div>
+</form>
+</main>
+</body>
+</html>
